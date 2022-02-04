605e7bdb44908a1497e514bc1ff69ae70101681f
assadipourk <ASSADIPOURK@GMAIL.COM>
2022-02-04 03:07:13 -0500

    day 3
    added a bunch of bootstrap, calorie api is running in the console but still need it to work for the user on the front end, this is the main concern for friday

---
+++ 14.5/public/javascript/api.js
@@ -0,0 +1,30 @@
+let totalCalories = 0;
+var query = 'steak rice'
+var myName = document.getElementById("myText").value
+
+
+document.getElementById("myButton").onclick = function() {
+var myName = document.getElementById("myText").value
+console.log(myName);
+}
+
+$.ajax({
+    method: 'GET',
+    url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
+    headers: { 'X-Api-Key': 'o7Vl+wl2wg6RAxxPA/meUw==OXz8mZ5Ksv3yqUiG' },
+    contentType: 'application/json',
+    success: function (result) {
+        const results = result.items
+        for (let i = 0; i < results.length; i++) {
+
+            const calories = results[i].calories
+            totalCalories = totalCalories + calories
+        }
+        console.log(totalCalories);
+
+        totalCalories = 0
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
